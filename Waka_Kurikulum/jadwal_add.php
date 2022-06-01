5a427858108cda8b216e4105539d0a4f5b9c6c6e
joshua-jojo <joshuasitompul2000@gmail.com>
2022-06-01 21:17:54 +0700

    tambah pelajaran

--- Waka_Kurikulum/jadwal_add.php
+++ Waka_Kurikulum/jadwal_add.php
@@ -10,6 +10,10 @@ $hari					= $_POST["hari"];
 $jam					= $_POST["jam"];
 $materijadwal  			= $_FILES['berkas']['name'];
 $file_tmp			= $_FILES['berkas']['tmp_name'];
+
+date_default_timezone_set("Asia/Jakarta");
+$materijadwal = "pelajaran_".$kelas."_".$guru."_".date('Y_m_d_H_i_s_').$materijadwal;
+
 $link = $_POST['linkvidcon'];
 move_uploaded_file($file_tmp, '../jadwal/' . $materijadwal);
 if ($add = mysqli_query($konek, "INSERT INTO jadwal(id_pelajaran, id_kelas, id_guru, jam, no_hari, link_vidcon, materijadwal) VALUES ( '$pelajaran', '$kelas', '$guru', '$jam', '$hari','$link','$materijadwal')")) {
